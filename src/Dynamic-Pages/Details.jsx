320467f0f30e8eb0ce34654f2f303191aebcdf53
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-31 10:40:35 -0500

    Details Page Start

--- src/Dynamic-Pages/Details.jsx
+++ src/Dynamic-Pages/Details.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Styles from "../Styles/Detail-Page/Details.module.css"
 
 const Details = () => {
   return (
-    <div>Details</div>
+    <div className={Styles.Page}>
+      <div className={Styles.Content_Container}>
+        <div className={Styles.Header_Image_Container}>
+          <img src={""} alt="" className={Styles.Header_Image} />
+        </div>
+
+        
+      </div>
+    </div>
   )
 }
 
